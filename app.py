b37e631cebf007da54613dd0e079a016e5519bbb
James Ko <jamesqko@gmail.com>
2020-04-17 15:24:26 -0400

    Lots of styling work

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
